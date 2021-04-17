5055437918cd23fc904db10ec3795de6ec4a65f6
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-04-17 21:14:49 +0300

    palindrome

--- cypress/integration/isPalindrome.spec.js
+++ cypress/integration/isPalindrome.spec.js
@@ -18,6 +18,11 @@ const p4 = 'Step on no pets'
 const p5 = 'Top spot'
 const p6 = 'No lemon, no melon'
 
+function palindrome(str) {
+    const p = str.toLowerCase().split("'").join('').split(',').join('').split(' ').join('');
+    return p === p.split('').reverse().join('');
+}
+
 describe('harmlessRansomNote', () => {
     it('should return true', () => {
         expect(isPalindrome(p1)).to.equal(true)
@@ -40,4 +45,29 @@ describe('harmlessRansomNote', () => {
     it('should return false', () => {
         expect(isPalindrome('san francisco')).to.equal(false)
     })
-})   
+});
+
+describe('harmlessRansomNote', () => {
+    it('should return true', () => {
+        expect(palindrome(p1)).to.equal(true)
+    })
+    it('should return true', () => {
+        expect(palindrome(p2)).to.equal(true)
+    })
+    it('should return true', () => {
+        expect(palindrome(p3)).to.equal(true)
+    })
+    it('should return true', () => {
+        expect(palindrome(p4)).to.equal(true)
+    })
+    it('should return true', () => {
+        expect(palindrome(p5)).to.equal(true)
+    })
+    it('should return true', () => {
+        expect(palindrome(p6)).to.equal(true)
+    })
+    it('should return false', () => {
+        expect(palindrome('san francisco')).to.equal(false)
+    })
+}); 
+
